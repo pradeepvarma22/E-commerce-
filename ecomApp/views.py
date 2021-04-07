0d6438bb76485fd0ae72bf94f600f36566867e34
varma7 <pradeepvarma107@gmail.com>
2021-04-06 19:44:52 -0700

    add : Machine Learning Recommendation alorithm Userbased Collaborative Filtering
    Products Based on Customer Rating given By customers
    
    Signed-off-by: varma7 <pradeepvarma107@gmail.com>

--- ecomApp/views.py
+++ ecomApp/views.py
@@ -4,8 +4,10 @@ from .models import *
 from accounts.decorators import customer_required,seller_required
 from django.contrib.auth.decorators import login_required
 from cart.cart import Cart
-
 from ecomApp.forms import OrderForm,RatingForm
+from django.db.models import Case, When
+import pandas as pd
+
 
 
 @login_required
@@ -58,7 +60,6 @@ def page_view(request,id):
     except:
         check_already_reviewd = 0
         check_already_reviewd_pro=0
-
         
     if check_already_reviewd and check_already_reviewd_pro:
         Flagg = True
@@ -151,3 +152,54 @@ def order(request):
                'total_price':total_price
                }
     return render(request, 'ecomApp/order.html',context)
+
+
+
+
+
+# To get similar Products based on user rating
+def get_similar(product_name,rating,corrMatrix):
+    similar_ratings = corrMatrix[product_name]*(rating-2)
+    similar_ratings = similar_ratings.sort_values(ascending=False)
+    return similar_ratings
+
+# Recommendation Algorithm
+def recommend(request):
+    
+    product_rating=pd.DataFrame(list(MyRating.objects.all().values()))
+
+    new_user=product_rating.user_id.unique().shape[0]
+    mainuser = request.user
+    customeruu = Customer.objects.get(user=mainuser)
+    current_user_id= customeruu.user.id
+
+	# if new user not rated any Product
+    if current_user_id>new_user:
+        product=Product.objects.get(id=25)      #Any object
+        q=MyRating(user=customeruu,product=product,rating=1)
+        q.save()
+
+    #product_rating['rating']=pd.to_numeric(product_rating)
+    convert_dict = {'rating': int } 
+    product_rating = product_rating.astype(convert_dict)
+    print(product_rating)
+    print(product_rating.dtypes)
+    userRatings = product_rating.pivot_table(index=['user_id'],columns=['product_id'],values='rating')
+    userRatings = userRatings.fillna(0,axis=1)
+    corrMatrix = userRatings.corr(method='pearson')
+
+    user = pd.DataFrame(list(MyRating.objects.filter(user=customeruu).values())).drop(['user_id','id'],axis=1)
+    user_filtered = [tuple(x) for x in user.values]
+    product_id_watched = [each[0] for each in user_filtered]
+
+    similar_products = pd.DataFrame()
+    for product,rating in user_filtered:
+        similar_products = similar_products.append(get_similar(product,int(rating),corrMatrix),ignore_index = True)
+
+    products_id = list(similar_products.sum().sort_values(ascending=False).index)
+    products_id_recommend = [each for each in products_id if each not in product_id_watched]
+    preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(products_id_recommend)])
+    product_list=list(Product.objects.filter(id__in = products_id_recommend).order_by(preserved)[:10])
+
+    context = {'product_list': product_list}
+    return render(request, 'ecomApp/recommend.html', context)
