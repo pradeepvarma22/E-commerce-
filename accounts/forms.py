0da64c426c3b8a2a5427b80ae5173504ab15caf5
shivaram33 <53346288+shivaram33@users.noreply.github.com>
2021-04-17 09:07:01 +0530

    update : Css by shivaram
    Signed-off-by: shivaram33 <53346288+shivaram33@users.noreply.github.com>

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ class AddProduct(forms.ModelForm):
     class Meta:
         model = Product
         exclude = ('user',)
-        fields=('name','price','image')
+        fields=('name','price','image','description',)
 
         widgets = {
             'image': forms.widgets.FileInput
--- ecomApp/models.py
+++ ecomApp/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255,null=True,blank=True)
     price = models.FloatField(null=True,blank=True)
     image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=3000,null=True,blank=True)
 
     def __str__(self):
         return self.name
